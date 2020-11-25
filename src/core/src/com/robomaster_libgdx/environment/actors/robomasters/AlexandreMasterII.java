2f9aac37db7df389f892542d3775987525a2bed0
AlexandrePinneauHe <3121465535@qq.com>
2020-11-26 02:30:55 +0800

    added paramers of cars

--- src/core/src/com/robomaster_libgdx/environment/actors/robomasters/AlexandreMasterII.java
+++ src/core/src/com/robomaster_libgdx/environment/actors/robomasters/AlexandreMasterII.java
@@ -3,18 +3,16 @@ package com.robomaster_libgdx.environment.actors.robomasters;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class AlexanderMasterI extends RoboMaster{
-    public AlexanderMasterI(){
+public class AlexandreMasterII extends RoboMaster{
+    public AlexandreMasterII(){
         super(new TextureRegion(
-                        new Texture("RoboMasters/Alexander's Master I.png")));
+                        new Texture("RoboMasters/Alexander's Master II.png")));
     }
     private double weight = 17.1;           //Kg
     private int max_forward_speed = 3;      //m/s
     private int max_cross_range_speed = 2;  //m/s
-    private float shooting_speed = 10;      //per second
+    private float shooting_speed = 6;       //per second
     private float cannon_range = 180;       //per second
     private float bullet_speed = 25;        //m/s
-    private int max_carrying_bullet = 200;  //m/s
-
-
+    private int max_carrying_bullet = 300;  //m/s
 }
